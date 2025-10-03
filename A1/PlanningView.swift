30904d57e3ec9dd918db1d6c7d245638a749fa5f
Student <student@Students-MacBook-Pro.local>
2025-10-03 16:51:49 -0400

    Created all 3 screens

---
+++ A1/PlanningView.swift
@@ -0,0 +1,9 @@
+//
+//  PlanningView.swift
+//  A1
+//
+//  Created by Student on 2025-10-03.
+//  Copyright © 2025 Robin Wagubi. All rights reserved.
+//
+
+import Foundation
---
+++ A1/SummaryView.swift
@@ -0,0 +1,9 @@
+//
+//  SummaryView.swift
+//  A1
+//
+//  Created by Student on 2025-10-03.
+//  Copyright © 2025 Robin Wagubi. All rights reserved.
+//
+
+import Foundation
