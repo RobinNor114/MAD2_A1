abad18479f48c7dd82632c383d0cc46ddf1ab3b2
Student <student@Students-MacBook-Pro.local>
2025-10-03 16:24:22 -0400

    first

---
+++ A1/HOME.swift
@@ -0,0 +1,16 @@
+
+//
+//  HomeViewController.swift
+//  A1
+//
+//  Created by Student on 2025-10-03.
+//  Copyright © 2025 Robin Wagubi. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ A1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  A1
+//
+//  Created by Student on 2025-10-02.
+//  Copyright © 2025 Robin Wagubi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
