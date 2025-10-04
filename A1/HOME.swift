5c0a126522432f4d9a4c84d57cb0c5405ea37ed2
Student <student@Students-MacBook-Pro.local>
2025-10-03 20:38:36 -0400

    new planning screen

--- A1/HOME.swift
+++ A1/HOME.swift
@@ -10,7 +10,20 @@
 import UIKit
 
 class HOME: UIViewController {
+    @IBOutlet weak var destinationImageView: UIImageView!
+    @IBOutlet weak var destinationLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Set destination
+        destinationImageView.image = UIImage(named: "greece")
+        destinationLabel.text = "Greece"
+        destinationLabel.textAlignment = .center
+        destinationLabel.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        
+        // Rounded corners for the image
+            destinationImageView.layer.cornerRadius = 20
+            destinationImageView.clipsToBounds = true
     }
 }
