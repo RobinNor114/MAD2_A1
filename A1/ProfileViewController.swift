63d13adc5d1188ea83450a33150f69a954077f24
kristinT <kristin.theoret@gmail.com>
2025-11-14 17:04:28 -0500

    Created a ProfileViewController swift file to modify the profile pictures shape

---
+++ A1/ProfileViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ProfileViewController.swift
+//  A1
+//
+//  Created by Kristin Theoret on 2025-11-14.
+//  Copyright © 2025 Robin Wagubi. All rights reserved.
+//
+
+
+//
+//  ProfileViewController.swift
+//  A1
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Round profile image
+        profileImageView.layer.cornerRadius = 60
+        profileImageView.clipsToBounds = true
+        profileImageView.layer.borderWidth = 2
+        profileImageView.layer.borderColor = UIColor.systemBlue.cgColor
+
+
+        // Gesture recognizer to dismiss keyboard
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+
+    @IBAction func saveTapped(_ sender: UIButton) {
+        print("Saved profile: \(nameTextField.text ?? "")")
+    }
+}
